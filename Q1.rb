e8abbb3b384e29b3d7d437b2755e941fef37cc73
Paul <ageoldpun@gmail.com>
2013-08-14 15:32:24 -0700

    Refactored Q1.rb

--- Q1.rb
+++ Q1.rb
@@ -4,15 +4,15 @@ include Minitest::Assertions
 # Q1: Write a method, pow, that takes two (non-negative, integer) numbers, base and exponent and returns base
 # raised to the exponent power.  (No fair using Ruby's base ** exponent notation!).
 def pow(base, exponent)
-  i = 1
   result = 1
-  while i <= exponent
-    result = result * base
-    i += 1
+  exponent.times do 
+    result = result * base 
   end
-  return result
+  result
 end
 assert_equal 8, pow(2, 3)
 assert_equal 9, pow(3, 2)
 assert_equal 81, pow(9, 2)
 assert_equal 256, pow(2, 8)
+assert_equal 1, pow(4, 0)
+assert_equal 5, pow(5, 1)
